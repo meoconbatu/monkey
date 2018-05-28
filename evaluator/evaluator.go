19f7b6f44cc70a4b5bd391f1fb8dcd87ffda4120
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2018-05-28 11:24:20 +0700

    evaluate Boolean Literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,14 +5,21 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
-	case *ast.IntegerLiteral:
-		return &object.Integer{Value: node.Value}
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -22,4 +29,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
